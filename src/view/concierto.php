b8cc592724f4060d3fe74d481dafca4115552cf3
Gerardo <gecifuen@alumnos.ubiobio.cl>
2024-05-16 19:58:32 -0400

    Cambiar el title.

--- src/view/concierto.php
+++ src/view/concierto.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Proyecto</title>
+    <title>Conciertos</title>
 
     <link rel="stylesheet" href="../css/bootstrap/bootstrap.css">
     <link rel="stylesheet" href="../css/fontawesome/css/all.min.css">
