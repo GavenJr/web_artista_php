cc8aa936c148015e6983c575aab6ab66f5e50481
Gerardo <gecifuen@alumnos.ubiobio.cl>
2024-05-16 19:10:21 -0400

    Añadir página de conciertos + menú.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Proyecto</title>
 
-    <link rel="stylesheet" href="./src/css/bootstrap.css">
+    <link rel="stylesheet" href="./src/css/bootstrap.min.css">
 
     <script src="./src/js/bootstrap.js"></script>
 
